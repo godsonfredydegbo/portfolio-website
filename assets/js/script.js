dad37c89286ef0cc587a0b19d649091a1e077f75
gson_s3ñ10r <gson_senior@Xunbutu.myguest.virtualbox.org>
2026-02-20 01:43:57 +0100

    ui on finish in mobile first

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,3 +28,28 @@ const shadowHeader = () => {
                          : header.classList.remove('shadow-header')
 }
 window.addEventListener('scroll', shadowHeader)
+
+/* ==================== EMAIL JS ===================== */
+const contactForm = document.getElementById('contact-form');
+const contactMessage = document.getElementById('contact-message');
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_fvd7nqr', 'template_17z06vj', '#contact-form', '0T4P-fLZqKXE6OLTm')
+    .then(() => {
+        contactMessage.textContent = 'Votre message a bien été envoyé'
+
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+
+        contactForm.reset()
+
+    }, () => {
+        contactMessage.textContent = "Votre message n'a pas été envoyé"
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
