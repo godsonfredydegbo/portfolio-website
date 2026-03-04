2d02b9a46039cc2dd1885e3e897adf39d41d85fd
gson_s3ñ10r <gson_senior@Xunbutu.myguest.virtualbox.org>
2026-03-04 22:05:03 +0100

    dark-theme on process

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,29 +92,36 @@ window.addEventListener('scroll', scrollActive)
 /* ==================== DARK LIGHT THEME ===================== */
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
-const iconTheme = 'sun-line'
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const moonIcon = 'assets/icons/tabler_moon.svg'
+const sunIcon = 'assets/icons/mi_sun-1.svg'
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'moon-line' : 'sun-line'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-    themeButton.classList[selectedIcon === 'moon-line' ? 'add' : 'remove'](iconTheme)
+// Function that displays the correct image according to the theme
+function updateIcon() {
+    if (document.body.classList.contains(darkTheme)) {
+        themeButton.src = sunIcon
+    }else {
+        themeButton.src = moonIcon
+    }
 }
 
-// Activate / deactivate the theme manually with the button
+//Load saved theme
+const savedTheme = localStorage.getItem('selected-theme')
+
+if (savedTheme === 'dark') {
+    document.body.classList.add(darkTheme)
+}
+
+updateIcon()
+
+//On clic
 themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
+
+    const currentTheme =
+        document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+
+    localStorage.setItem('selected-theme', currentTheme)
+
+    updateIcon()
 })
