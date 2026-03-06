973b299684a28564b28f933f39605df0b5907983
gson_s3ñ10r <gson_senior@Xunbutu.myguest.virtualbox.org>
2026-03-06 14:19:42 +0100

    Breakpoints and aniations finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,3 +140,18 @@ themeButton.addEventListener('click', () => {
     updateIcon()
     updateLogo()
 })
+
+/* ================ SCROLL REVEAL ANIMATION ================ */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true // Animations repeat
+})
+
+sr.reveal(`.home_perfil, .about_image, .contact_mail`, {origin: 'right'})
+sr.reveal(`.home_name, .home_info,
+           .about_container .section_title-1, .about_info, 
+           .contact_social, .contact_data`, {origin: 'left'})
+sr.reveal(`.services_card, .projects_card`, {interval: 200})
